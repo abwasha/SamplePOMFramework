6c19a34775e338e04c6d168a1250f84509575376
ABDUL WAHAB SHARIFF <abwasha@outlook.com>
2023-08-18 14:43:20 +0530

    Clean Up

--- src/test/java/tests/ContactFormTests/DivergeMergeTest.java
+++
@@ -1,5 +0,0 @@
-package tests.ContactFormTests;
-
-public class DivergeMergeTest {
-    //Create a DivergeMerge Branch from the Main and add a change(DivergeMergeTest file) and commit
-}
--- src/test/java/tests/ContactFormTests/MainBranchTest.java
+++
@@ -1,6 +0,0 @@
-package tests.ContactFormTests;
-
-public class MainBranchTest {
-
-    //On the Main branch add a change (MainBranchTest file) and commit
-}
